7e71e3c7423fd168193542143e483cf01261765b
Jaesuru <jasongsm2017@gmail.com>
2025-10-22 18:25:34 -0400

    Add base functions for computer/human responses
    - Implement randomized computer response using
      Math.random().
    - Add human response using window.prompt().
    - Set up initial game structure in index.html and
      script.js.

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Hello, world!");
+
+// The getComputerChoice function will randomly pick either 
+// "rock", "paper", or "scissors" as one of its options and return that value.
+function getComputerChoice() {
+    const randomizer = Math.floor(Math.random() * 3) + 1;
+
+    switch(randomizer) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+        default:
+            return "error";
+    }
+}
+
+// console.log(getComputerChoice());
+
+// And then this awesome little function will ask the user for their input, and store it
+// as a variable for later usage. 
+function getHumanChoice() {
+    const userInput = window.prompt("Choose your move...");
+    return userInput.toString();
+}
+
+// console.log(getHumanChoice());
